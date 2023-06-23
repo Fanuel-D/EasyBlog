cb2318527c1ecd3d5392f649c300984d69d37e85
Fanuel Dana <110620678+FdD1223@users.noreply.github.com>
2023-06-23 23:56:13 +0300

    Mongo

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from pymongo import MongoClient
+import datetime
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://fanuel:Kbsdaf123!@cluster0.xzjgpxo.mongodb.net/")
 
-@app.route("/")
+
+
+entries = []
+@app.route("/", methods = ["GET", "POST"])
 
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        content = request.form.get("post")
+        date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((content, date))
+    entries_format = [
+        (entry[0],
+        entry[1],
+        datetime.datetime.strptime(entry[1],"%Y-%m-%d").strftime("%b %d"))
+        for entry in entries
+    ]
+    return render_template("home.html", entries = entries_format)
