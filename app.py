9bce35224863024c2dda5e5e73049f6efac940ed
Fanuel Dana <110620678+FdD1223@users.noreply.github.com>
2023-06-24 12:38:04 +0300

    BackEnd Kinda done

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template,request
 from pymongo import MongoClient
 import datetime
+import certifi
 app = Flask(__name__)
-client = MongoClient("mongodb+srv://fanuel:Kbsdaf123!@cluster0.xzjgpxo.mongodb.net/")
+client = MongoClient("mongodb+srv://fanuel:Kbsdaf123!@cluster0.xzjgpxo.mongodb.net/", tlsCAFile=certifi.where())
+app.db = client.myblog
 
 
 
-entries = []
+
 @app.route("/", methods = ["GET", "POST"])
 
 def home():
+
     if request.method == "POST":
         content = request.form.get("post")
         date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((content, date))
+        app.db.entries.insert_one({"post":content, "date":date})
     entries_format = [
-        (entry[0],
-        entry[1],
-        datetime.datetime.strptime(entry[1],"%Y-%m-%d").strftime("%b %d"))
-        for entry in entries
+        (entry["post"],
+        entry["date"],
+        datetime.datetime.strptime(entry["date"],"%Y-%m-%d").strftime("%b %d"))
+        for entry in app.db.entries.find({})
     ]
     return render_template("home.html", entries = entries_format)
