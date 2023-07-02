81dc69256484686725ecc774d2b1e23187b1e48c
Fanuel Dana <110620678+FdD1223@users.noreply.github.com>
2023-07-02 20:49:52 +0300

    Small Changes

--- app.py
+++ app.py
@@ -1,38 +1,48 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import datetime
 import certifi
 
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://fanuel:Kbsdaf123!@cluster0.xzjgpxo.mongodb.net/", tlsCAFile=certifi.where())
+app.db = client.myblog
 
-def create_app():
-    app = Flask(__name__)
-    client = MongoClient("mongodb+srv://fanuel:Kbsdaf123!@cluster0.xzjgpxo.mongodb.net/", tlsCAFile=certifi.where())
-    app.db = client.myblog
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        content = request.form.get("post")
+        date = datetime.datetime.today().strftime("%Y-%m-%d")
+        app.db.entries.insert_one({"post": content, "date": date})
+
+    entries_format = [
+        (entry["post"], entry["date"], datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"))
+        for entry in app.db.entries.find({})
+    ]
+    return render_template("home.html", entries=entries_format)
 
 
+@app.route("/calendar")
+def calendar():
+    return render_template('cal.html')
 
 
-    @app.route("/", methods = ["GET", "POST"])
-    def home():
+@app.route("/posts")
+def get_posts():
+    date = request.args.get('date')
+    posts = app.db.entries.find({"date": date})
+    response = []
+    for post in posts:
+        response.append({
+            "post": post["post"],
+            "date": post["date"]
+        })
+    return jsonify(response)
 
-        if request.method == "POST":
-            content = request.form.get("post")
-            date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert_one({"post":content, "date":date})
-        entries_format = [
-            (entry["post"],
-            entry["date"],
-            datetime.datetime.strptime(entry["date"],"%Y-%m-%d").strftime("%b %d"))
-            for entry in app.db.entries.find({})
-        ]
-        return render_template("home.html", entries = entries_format)
-    @app.route("/calendar")
-    def calendar():
-     
-        return render_template('cal.html')
 
-    @app.route("/bookmarks")
-    def bookmarks():
-       
-        return render_template('bookmarks.html')
-    return app
+@app.route("/bookmarks")
+def bookmarks():
+    return render_template('bookmarks.html')
+
+
+if __name__ == '__main__':
+    app.run()
